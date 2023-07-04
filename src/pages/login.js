8bb6bad5656c9554fedbf471156e20f595d1c337
Aman Shinde <1703amanshinde98@gmail.com>
2023-07-04 21:48:22 +0530

    Add notification

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,45 @@
 import styles from "../styles/login.module.css";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggingIn, setLoggingIn] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      toast.error("Please enter both email and password.", {
+        duration: 4000,
+        position: "top-center",
+      });
+      return;
+    }
+    setLoggingIn(true);
+  };
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}></span>
       <div className={styles.field}>
-        <input type="email" value="" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.email)}
+        />
       </div>
       <div className={styles.field}>
-        <input type="password" value="" placeholder="Password" required />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className={styles.field}>
-        <button>Login</button>
+        <button disabled={loggingIn}>
+          {loggingIn ? "Loging..." : "Login"}
+        </button>
       </div>
     </form>
   );
